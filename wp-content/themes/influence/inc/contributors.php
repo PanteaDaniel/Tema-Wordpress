31ebf6c215496eb498bfa42f35a98bd01eac531b
Daniel Pantea <danie@Daneil.com>
2018-08-02 14:55:43 +0300

    Am adaugat o tema si child-ul ei

---
+++ wp-content/themes/influence/inc/contributors.php
@@ -0,0 +1,10 @@
+<?php
+return json_decode( '{
+	"a885c7bd5442dd2acd8c3e42001332c6": {
+		"name": "Alex S",
+		"email": "a885c7bd5442dd2acd8c3e42001332c6",
+		"loc": 2,
+		"score": 0.18608304542903512,
+		"percent": 100
+	}
+}', true );
